31ffbfa58bc5860988a90fbc74bb03b3f03de3b6
jgromes <jgromes@users.noreply.github.com>
2019-07-26 09:28:14 +0200

    Added CMD_RETRANSMIT_CUSTOM frame

--- examples/RepeatMessageCustom/RepeatMessageCustom.ino
+++ examples/RepeatMessageCustom/RepeatMessageCustom.ino
@@ -2,6 +2,17 @@
 #include <FOSSA-Comms.h>
 #include <RadioLib.h>
 
+// modulation properties to be used in transmission
+#define BANDWIDTH               5   // 41.7 kHz - see array below
+#define SPREADING_FACTOR        12  // SF 12
+#define CODING_RATE             7   // CR 4/7
+#define PREAMBLE_LENGTH         32  // symbols
+#define CRC_ENABLED             1
+#define OUTPUT_POWER            10  // dBm
+
+// array of allowed bandwidth values in kHz
+float bws[] = {7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0, 500.0};
+
 // SX1262 has the following connections:
 // NSS pin:   10
 // DIO1 pin:  2
@@ -36,7 +47,7 @@ void setup() {
 
   // initialize SX1262
   Serial.print(F("Initializing ... "));
-  int state = radio.begin(434.0, 125.0, 11, 8, 0x0F0F);
+  int state = radio.begin(434.0, 125.0, 11, 8, 0x0F0F, 21, 60, 16);
   if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -78,15 +89,29 @@ void loop() {
     Serial.println(F("Transmitting packet ... "));
 
     // data to transmit
-    uint8_t functionId = CMD_TRANSMIT_CALLSIGN_LOW_POWER;
+    uint8_t functionId = CMD_RETRANSMIT_CUSTOM;
+    char message[] = "I'm a message!";
+    uint8_t optDataLen = 7 + strlen(message);
+    uint8_t* optData = new uint8_t[optDataLen];
+    optData[0] = BANDWIDTH;
+    optData[1] = SPREADING_FACTOR;
+    optData[2] = CODING_RATE;
+    optData[3] = (uint8_t)(PREAMBLE_LENGTH & 0xFF);
+    optData[4] = (uint8_t)((PREAMBLE_LENGTH >> 8) & 0xFF);
+    optData[5] = CRC_ENABLED;
+    optData[6] = (uint8_t)OUTPUT_POWER;
+    memcpy(optData + 7, message, strlen(message));
 
     // build frame
-    uint8_t len = FCP_Get_Frame_Length(callsign);
+    uint8_t len = FCP_Get_Frame_Length(callsign, optDataLen);
     uint8_t* frame = new uint8_t[len];
-    FCP_Encode(frame, callsign, functionId);
+    FCP_Encode(frame, callsign, functionId, optDataLen, optData);
+    delete[] optData;
     PRINT_BUFF(frame, len);
 
-    // send data
+    // send data with default configuration
+    radio.begin(434.0, 125.0, 11, 8, 0x0F0F, 21, 60, 16);
+    radio.setCRC(true);
     int state = radio.transmit(frame, len);
     delete[] frame;
 
@@ -95,8 +120,10 @@ void loop() {
       Serial.println(F("Success!"));
     }
 
-    // set radio mode to reception with low SF
+    // set radio mode to reception with custom configuration
     Serial.println(F("Waiting for response ... "));
+    radio.begin(434.0, bws[BANDWIDTH], SPREADING_FACTOR, CODING_RATE, 0x0F0F, OUTPUT_POWER, 60, PREAMBLE_LENGTH);
+    radio.setCRC(CRC_ENABLED);
     radio.setDio1Action(setFlag);
     radio.startReceive();
     enableInterrupt = true;
@@ -126,8 +153,23 @@ void loop() {
       Serial.print(F("Function ID: 0x"));
       Serial.println(functionId, HEX);
 
-      if(functionId == RESP_CALLSIGN_LOW_POWER) {
-        Serial.println(F("Got low power callsign!"));
+      if(functionId == RESP_REPEATED_MESSAGE_CUSTOM) {
+        Serial.println(F("Got response with custom configuration!"));
+
+        // check optional data
+        uint8_t respOptDataLen = FCP_Get_OptData_Length(callsign, respFrame, respLen);
+        if(respOptDataLen > 0) {
+          // frame contains optional data
+          uint8_t* respOptData = new uint8_t[respOptDataLen];
+          FCP_Get_OptData(callsign, respFrame, respLen, respOptData);
+  
+          // print optional data
+          Serial.print(F("Optional data ("));
+          Serial.print(respOptDataLen);
+          Serial.println(F(" bytes):"));
+          PRINT_BUFF(respOptData, respOptDataLen);
+          delete[] respOptData;
+        }
       }
       
     } else {
